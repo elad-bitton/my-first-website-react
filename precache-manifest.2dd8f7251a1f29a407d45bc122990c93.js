5338bb705ac7ad941758d1ab464bde2c063c7c15
elad-bitton <elad@thebitton.net>
2020-06-20 14:39:02 +0300

    Updates

---
+++ precache-manifest.2dd8f7251a1f29a407d45bc122990c93.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ebc9cabd8cd9a0f9cb89337c6fbab4",
+    "url": "/elad-bitton/my-first-website-react/index.html"
+  },
+  {
+    "revision": "bfc072af5be09e19cb29",
+    "url": "/elad-bitton/my-first-website-react/static/css/main.c7b00dd3.chunk.css"
+  },
+  {
+    "revision": "3e9cdc64e1490ee68793",
+    "url": "/elad-bitton/my-first-website-react/static/js/2.93f0115d.chunk.js"
+  },
+  {
+    "revision": "cb4293a028d0175415ddb31dc1aaaefb",
+    "url": "/elad-bitton/my-first-website-react/static/js/2.93f0115d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc072af5be09e19cb29",
+    "url": "/elad-bitton/my-first-website-react/static/js/main.85d7cc61.chunk.js"
+  },
+  {
+    "revision": "d0e11711d08dbcc12b37",
+    "url": "/elad-bitton/my-first-website-react/static/js/runtime-main.99863fec.js"
+  },
+  {
+    "revision": "c6802f7f5b10299bbd3080120ba004ab",
+    "url": "/elad-bitton/my-first-website-react/static/media/Login.c6802f7f.svg"
+  },
+  {
+    "revision": "acf1332f936eef348386cc03e1680879",
+    "url": "/elad-bitton/my-first-website-react/static/media/Welcome.acf1332f.svg"
+  },
+  {
+    "revision": "98215dde21adb3f51c597360d9e6d5b3",
+    "url": "/elad-bitton/my-first-website-react/static/media/cat_BG.98215dde.jpg"
+  }
+]);
